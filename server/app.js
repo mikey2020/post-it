0e1ba50000e7b84e565fde9be09b28010c95fc25
Michael Eboagu <Michael Eboagu>
2017-06-11 22:31:23 +0100

    Set babel to transpile es6/es2015 to es5

--- server/app.js
+++ server/app.js
@@ -1,15 +1,39 @@
+'use strict';
+
 //require('dotenv').config();
-import * as babel from 'babel-core';
-babel.transform("code", options);
-import * as dotenv from 'dotenv';
 
-import * as express from 'express';
-dotenv.config();
+//import * as babel from 'babel-core';
 
+//babel.transform("code();", options);
 
+//import * as dotenv from 'dotenv'
+
+import express from 'express';
 
 const app = express();
 
-app.listen(3000, function() {
+app.use( (req, res, next) =>  {
+  let err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+
+  res.status(err.status || 500);
+  console.log(err);
+});
+
+const port = process.env.PORT || 3000 ;
+
+app.listen(port, () => {
   console.log('Listening on port 3000...')
 });
