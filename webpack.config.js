03d3949c041e987b42dff19afcbd1a55ee9b111e
Michael Eboagu <Michael Eboagu>
2017-06-29 00:25:00 +0100

    Update webpack config file for production

--- webpack.config.js
+++ webpack.config.js
@@ -21,12 +21,26 @@ export default {
 		new webpack.NoEmitOnErrorsPlugin(),
 		new webpack.optimize.OccurrenceOrderPlugin(),
 		new webpack.HotModuleReplacementPlugin(),
+	    new webpack.optimize.UglifyJsPlugin({
+	      beautify: false,
+	      mangle: {
+	        screw_ie8: true,
+	        keep_fnames: true
+	      },
+	      compress: {
+	        screw_ie8: true
+	      },
+	      comments: false
+	    }),
+
+	    new webpack.LoaderOptionsPlugin({
+	      minimize: true,
+	      debug: false
+	    }),
+
 	    new webpack.DefinePlugin({
-		  'process.env': {
-		    NODE_ENV: JSON.stringify('production')
-		  }
-		}),
-		new webpack.optimize.UglifyJsPlugin()
+	      'process.env.NODE_ENV': JSON.stringify('production')
+	    })
 	],
 
 	module: {
