a1822ce63d0e4ceaea1bc974efde75edf2c5bd42
Michael Eboagu <Michael Eboagu>
2017-06-28 12:45:27 +0100

    Update groupContoller.js

--- server/controllers/groupController.js
+++ server/controllers/groupController.js
@@ -59,8 +59,8 @@ class GroupActions {
 						  .catch((err) => {
 							  console.log(err);
 							  res.json({ message: 'error saving to database' });
-							  
-						res.json({ message: 'user added to group' });
+
+						res.status(200).json({ message: 'user added to group' });
 	  }));
 
 	  });
