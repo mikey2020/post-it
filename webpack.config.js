4ff79be781b252220d7c525039e56f18d0921264
Michael Eboagu <Michael Eboagu>
2017-06-28 23:54:33 +0100

    Update webpack config file for production

--- webpack.config.js
+++ webpack.config.js
@@ -35,6 +35,9 @@ export default {
 	        screw_ie8: true
 	      },
 	      comments: false
+	    }),
+	    new webpack.DefinePlugin({
+	      'process.env.NODE_ENV': JSON.stringify('production')
 	    })
 	],
 
