e9a23d6221af85484f50da6fa6567b698567fd4c
Michael Eboagu <Michael Eboagu>
2017-06-29 13:29:55 +0100

    Change gulplif.js to gulpfile.babel.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,22 +2,30 @@ import coveralls from 'gulp-coveralls';
 import istanbul from 'gulp-istanbul';
 import gulp from 'gulp';
 import mocha from 'gulp-mocha';
+import babel from 'gulp-babel';
+import path from 'path';
 
-gulp.task('coveralls', ['test'], function() {
+/*gulp.task('coveralls', ['test'], function() {
     // lcov.info is the file which has the coverage information we wan't to upload
     return gulp.src(__dirname + '/coverage/lcov.info')
       .pipe(coveralls());
-});
+});*/
+
+gulp.task('transpile', () =>
+  gulp.src(['server/test/**/*.js', '!server/dist/**'])
+    .pipe(babel())
+    .pipe(gulp.dest('server/dist'))
+);
 
 gulp.task('pre-test', () =>
-  gulp.src(['server/test/**/*.js'])
+  gulp.src(['server/dist/**/*.js'])
     .pipe(istanbul())
     .pipe(istanbul.hookRequire())
 );
 
 gulp.task('test', ['pre-test'], () =>
-  gulp.src([path.join('server', 'test')])
-    .pipe(mocha({reporter: 'spec'}))
+  gulp.src([path.join('server', 'dist')])
+    .pipe(mocha())
     .pipe(istanbul.writeReports())
 );
 
@@ -26,3 +34,5 @@ gulp.task('coverage', ['test'], () => {
     .pipe(coveralls())
     .pipe(exit());
 });
+
+
