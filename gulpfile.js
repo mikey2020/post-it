a53c54a938781ef37510b71e2bdf8b23802a0392
Michael Eboagu <Michael Eboagu>
2017-06-29 12:36:43 +0100

    Update gulpfile for coveralls

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,25 @@
 import coveralls from 'gulp-coveralls';
-
+import istanbul from 'gulp-istanbul';
 import gulp from 'gulp';
+import mocha from 'gulp-mocha';
+
+gulp.task('coveralls', ['test'], function() {
+    // lcov.info is the file which has the coverage information we wan't to upload
+    return gulp.src(__dirname + '/coverage/lcov.info')
+      .pipe(coveralls());
+});
+
+gulp.task('pre-test', () =>
+  gulp.src(['server/test/**/*.js'])
+    .pipe(istanbul())
+    .pipe(istanbul.hookRequire())
+);
+
+gulp.task('test', ['pre-test'], () =>
+  gulp.src([path.join('server', 'test')])
+    .pipe(mocha({reporter: 'spec'}))
+    .pipe(istanbul.writeReports())
+);
 
 gulp.task('coverage', ['test'], () => {
   gulp.src('coverage/**/lcov.info')
