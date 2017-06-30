cb936c54f74e063996243212c30f15ce2b080b24
Michael Eboagu <Michael Eboagu>
2017-06-30 04:11:11 +0100

    Refactor code to pass eslint tests

--- server/test/routesTest.js
+++ server/test/routesTest.js
@@ -22,81 +22,66 @@ describe('Test api routes', () => {
   describe(' All routes should work', () => {
     it('should return "test-user is valid"', (done) => {
       user.post('/api/user/signin')
-																				.send({ username: 'test-user', password: 'pass' })
-																				.end((err, res) => {
+			.send({ username: 'test-user', password: 'pass' })
+			.end((err, res) => {
   res.status.should.equal(200);
   should.not.exist(err);
-																								// res.body.error.should.be.a.object();
   res.body.should.have.property('user', res.body.user);
-
   done();
 });
     });
 
     it('should return "test-group  successfully created" ', (done) => {
       user.post('/api/group')
-																.send({ name: 'test-group' })
-																.end((err, res) => {
+				.send({ name: 'test-group' })
+				.end((err, res) => {
   res.status.should.equal(200);
   should.not.exist(err);
-																				// res.body.error.should.be.a.object();
   res.body.should.have.property('message', res.body.message);
-
-
   done();
 });
     });
 
     it('should return "user added to group" ', (done) => {
       user.post('/api/group/1/user')
-																.send({ username: 'user3' })
-																.end((err, res) => {
+				.send({ username: 'user3' })
+				.end((err, res) => {
   res.status.should.equal(200);
   should.not.exist(err);
-																				// res.body.error.should.be.a.object();
   res.body.should.have.property('message', res.body.message);
-
-
   done();
 });
     });
 
     it('should return "message posted to group" ', (done) => {
       user.post('/api/group/1/message')
-																				.send({ message: 'how is everybody doing?' })
-																				.end((err, res) => {
+				.send({ message: 'how is everybody doing?' })
+				.end((err, res) => {
   res.status.should.equal(200);
   should.not.exist(err);
-																								// res.body.error.should.be.a.object();
   res.body.should.have.property('message', res.body.message);
-
-
   done();
 });
     });
 
     it('should return messages posted to group ', (done) => {
       user.get('/api/group/1/messages')
-																				// .expect(200)
-																				.end((err, res) => {
-  res.status.should.equal(200);
-  should.not.exist(err);
-																								// res.body.error.should.be.a.object();
-  res.body.should.have.property('posts', res.body.posts);
-
-
-  done();
-});
+		  	.end((err, res) => {
+    res.status.should.equal(200);
+    should.not.exist(err);
+    res.body.should.have.property('posts', res.body.posts);
+    done();
+  });
     });
 
     it('should return group created by test-user', (done) => {
       user.get('/api/groups/test-user')
-																				.end((err, res) => {
-																		  res.status.should.equal(200);
-																		  should.not.exist(err);
+				.end((err, res) => {
+  res.status.should.equal(200);
+  should.not.exist(err);
 
 
- 																		 done();
+  done();
 });
     });
   });
@@ -104,54 +89,36 @@ describe('Test api routes', () => {
   describe('All routes should not work without login', () => {
     it('should not return "test-group  successfully created" ', (done) => {
       request(app).post('/api/group')
-																				.send({ name: 'test-group' })
-																				.end((err, res) => {
-  																					res.status.should.equal(401);
-  																					res.body.should.have.property('errors', res.body.errors);
-
-
-  																					done();
+				.end((err, res) => {
+  res.status.should.equal(401);
+  res.body.should.have.property('errors', res.body.errors);
+  done();
 });
     });
 
     it('should not return "user added to group" ', (done) => {
       request(app).post('/api/group/1/user')
-																.send({ username: 'user3' })
-																.end((err, res) => {
+				.end((err, res) => {
   res.status.should.equal(401);
-																				// should.exist(err);
-																				// res.body.error.should.be.a.object();
   res.body.should.have.property('errors', res.body.errors);
-
-
   done();
 });
     });
 
     it('should not return "message posted to group" ', (done) => {
       request(app).post('/api/group/1/message')
-																				.send({ message: 'how is everybody doing?' })
-																				.end((err, res) => {
+				.end((err, res) => {
   res.status.should.equal(401);
-																								// should.exist(err);
-																								// res.body.error.should.be.a.object();
   res.body.should.have.property('errors', res.body.errors);
-
-
   done();
 });
     });
 
     it('should not return messages posted to group ', (done) => {
       request(app).get('/api/group/1/messages')
-																				// .expect(200)
-																				.end((err, res) => {
+				.end((err, res) => {
   res.status.should.equal(401);
-																								// should.exist(err);
-																								// res.body.error.should.be.a.object();
   res.body.should.have.property('errors', res.body.errors);
-
-
   done();
 });
     });
@@ -170,12 +137,12 @@ describe('Test api routes', () => {
       }
     });
 
-    Post.destroy({
-      where: {
-        groupId: 1,
-        post: 'how is everybody doing?'
-      }
-    });
+	  Post.destroy({
+    where: {
+      groupId: 1,
+      post: 'how is everybody doing?'
+    }
+  });
 
     UserGroups.destroy({
       where: {
