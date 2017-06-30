7b6bb495f64f78126487b1a01cba26470331bee8
Michael Eboagu <Michael Eboagu>
2017-06-30 01:41:20 +0100

    Update coveralls.yml file

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,6 @@
-import {sequelize} from '../db.js';
+import { sequelize } from '../db.js';
 
-import {User,hashPassword,bcrypt} from '../models/models.js';
+import { User, hashPassword, bcrypt } from '../models/models.js';
 
 import Validations from '../middlewares/validations.js';
 
@@ -8,113 +8,88 @@ const validate = new Validations();
 
 class UserActions {
 
-	signup(req,res){
+  signup(req, res) {
+	 const { errors, isValid } = validate.signup(req.body);
 
-		const {errors,isValid} = validate.signup(req.body);
 
-
-		if(!isValid){
-			res.status(400).json(errors);
-		}
-
-		else{
+	 if (!isValid) {
+   res.status(400).json(errors);
+	 } else {
 			// force: true will drop the table if it already exists
-			
-			User.sync({force: false}).then(() => {
+
+   User.sync({ force: false }).then(() =>
 			  // Table created
-			  return User.create({
-				userName: req.body.username,
-				email: req.body.email,
-				password: req.body.password
+				User.create({
+	  userName: req.body.username,
+	  email: req.body.email,
+	  password: req.body.password
 			  })
 			  .catch((err) => {
 				  console.log(err);
-				  res.json({ message : "error saving to database"});
-				});
-			});
+				  res.json({ message: 'error saving to database' });
+  }));
 
-			res.json({  message:  req.body.username + ' successfully added' });
-		}
-		
-	}
+   res.json({ message: `${req.body.username} successfully added` });
+	 }
+  }
 
-	signin(req,res){
-	
-		req.session.username = req.body.username;
+  signin(req, res) {
+	 req.session.username = req.body.username;
 
-		User.findAll({
-			where: {
-				userName: req.body.username
-			}
-		})
+	 User.findAll({
+   where: {
+		  userName: req.body.username
+   }
+	 })
 		.then((user) => {
+  let data = JSON.stringify(user);
 
-		   let data = JSON.stringify(user);
-
-		   data = JSON.parse(data);
+  data = JSON.parse(data);
 
 
-		  if(bcrypt.compareSync(req.body.password, data[0].password) === true){
+		  if (bcrypt.compareSync(req.body.password, data[0].password) === true) {
+    console.log(data[0].id);
+    req.session.name = req.body.username;
+    req.session.userId = data[0].id;
 
-		  	console.log(data[0].id);
-		  	req.session.name = req.body.username ;
-		  	req.session.userId = data[0].id;
-
-			res.json({ user: { name: req.body.username , message:  req.body.username + ' signed in' } });
-
+	 res.json({ user: { name: req.body.username, message: `${req.body.username} signed in` } });
+		  }		  else {
+	 res.status(401).json({ errors: { form: 'Invalid Signin Parameters' } });
 		  }
-
-		  else{
-
-			res.status(401).json({errors: { form: "Invalid Signin Parameters"}});
-
-		  }
-		 
-		})
+})
 
 		.catch((err) => {
-			console.log(err);
-			res.status(404).json({ errors: { message: "Invalid login parameters"} });
-		})
+  console.log(err);
+  res.status(404).json({ errors: { message: 'Invalid login parameters' } });
+});
+  }
 
-	}
-
-	isUnique(req,res){
-		
-		User.findOne({
+  isUnique(req, res) {
+	 User.findOne({
 
 		  where: {
-		   userName: req.params.name
+    userName: req.params.name
 		  }
 
-		}).then((user) => {
-			res.json({user});
-		});
-	}
+	 }).then((user) => {
+   res.json({ user });
+	 });
+  }
 
 
-	allUsers(req,res){
-
-		User.findAll({}).then((data) => {
-			
-			res.json({data});
-
-		}).catch((err) => {
-			console.log(err);
-			res.json({message : "Error occured please try again"})
-		});
-	}
-
+  allUsers(req, res) {
+	 User.findAll({}).then((data) => {
+   res.json({ data });
+	 }).catch((err) => {
+   console.log(err);
+   res.json({ message: 'Error occured please try again' });
+	 });
+  }
 
 
 }
 
 
-
-
-
-
-
-export default UserActions ;
-//export {signup,allUsers,signin,isUnique} ;
+export default UserActions;
+// export {signup,allUsers,signin,isUnique} ;
 
