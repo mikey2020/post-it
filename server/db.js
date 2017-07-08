e6e1022a2cc2104d9aa3bc16b2ef1f7f8d2a0836
Andela Developer <superdafe@gmail.com>
2017-07-08 22:47:19 +0100

    Fix eslint errors in db.js file

--- server/db.js
+++ server/db.js
@@ -11,19 +11,16 @@ const sequelize = new Sequelize(process.env.DATABASE_URL);
 if (process.env.NODE_ENV === 'production') {
   pg.defaults.ssl = true;
 
-  	pg.connect(process.env.DATABASE_URL, (err, client) => {
+  pg.connect(process.env.DATABASE_URL, (err, client) => {
     if (err) throw err;
 
-    console.log('Connected to postgres! Getting schemas...');
-
     client
-			.query('SELECT table_schema,table_name FROM information_schema.tables;')
-			.on('row', (row) => {
-  			console.log(JSON.stringify(row));
-	});
-  
+      .query('SELECT table_schema,table_name FROM information_schema.tables;')
+      .on('row', (row) => {
+        JSON.stringify(row);
+      });
   });
 }
 
 
-export { sequelize };
+export default sequelize;
