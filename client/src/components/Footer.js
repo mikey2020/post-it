c2f715f1049258bc5e600ed56ecfd9d33c1a3bdb
Andela Developer <superdafe@gmail.com>
2017-08-07 16:15:56 +0100

    Set footer to be fixed on page

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default () => {
   return (
-        <footer className="page-footer red darken-1">
+        <footer className="page-footer red darken-1 my-footer">
           <div className="footer-copyright">
             <div className="container">
             © 2017 Copyright Text
